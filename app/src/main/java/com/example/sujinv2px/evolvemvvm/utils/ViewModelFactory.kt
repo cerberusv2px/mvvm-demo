ce236fc5c4a60bf888d07857bdf7cdb32f8ed60e
Sujin <sujin.v2px@gmail.com>
2018-06-04 17:54:38 +0545

    temo changes

--- app/src/main/java/com/example/sujinv2px/evolvemvvm/utils/ViewModelFactory.kt
+++ app/src/main/java/com/example/sujinv2px/evolvemvvm/utils/ViewModelFactory.kt
@@ -9,6 +9,7 @@ import javax.inject.Provider
 import javax.inject.Singleton
 import kotlin.reflect.KClass
 
+@Suppress("UNCHECKED_CAST")
 @PerActivity
 class ViewModelFactory @Inject constructor(private val viewModels:MutableMap<Class<out ViewModel>, Provider<ViewModel>>): ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
