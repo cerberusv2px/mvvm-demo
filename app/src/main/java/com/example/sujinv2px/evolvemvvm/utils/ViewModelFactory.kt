751c3b8e30ab5c28b92e83c782e0641e909cb5f8
Sujin <sujin.v2px@gmail.com>
2018-06-05 12:03:05 +0545

    Viewmodel factory class check

--- app/src/main/java/com/example/sujinv2px/evolvemvvm/utils/ViewModelFactory.kt
+++ app/src/main/java/com/example/sujinv2px/evolvemvvm/utils/ViewModelFactory.kt
@@ -3,16 +3,25 @@ package com.example.sujinv2px.evolvemvvm.utils
 import android.arch.lifecycle.ViewModel
 import android.arch.lifecycle.ViewModelProvider
 import com.example.sujinv2px.evolvemvvm.di.scope.PerActivity
-import dagger.MapKey
 import javax.inject.Inject
 import javax.inject.Provider
-import javax.inject.Singleton
-import kotlin.reflect.KClass
 
 @Suppress("UNCHECKED_CAST")
 @PerActivity
-class ViewModelFactory @Inject constructor(private val viewModels:MutableMap<Class<out ViewModel>, Provider<ViewModel>>): ViewModelProvider.Factory {
+class ViewModelFactory @Inject constructor(private val viewModels: MutableMap<Class<out ViewModel>, Provider<ViewModel>>) : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        return viewModels[modelClass]?.get() as T
+        var creator: Provider<ViewModel>? = viewModels[modelClass]
+        if (creator == null) {
+            for ((key, value) in viewModels) {
+                creator = value
+                break
+            }
+        }
+        if (creator == null) throw IllegalArgumentException("Unknown model class:$modelClass")
+        try {
+            return creator.get() as T
+        } catch (e: Exception) {
+            throw RuntimeException(e)
+        }
     }
 }
